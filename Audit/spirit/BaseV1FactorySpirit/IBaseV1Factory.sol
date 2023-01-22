a8d60efc3ccaa6baf914ac6c62fdc4e8c0bef125
AureumVictoria <54036863+AureumVictoria@users.noreply.github.com>
2023-01-23 00:59:07 +0100

    Delete Audit/spirit directory

--- Audit/spirit/BaseV1FactorySpirit/IBaseV1Factory.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.11;
-
-interface IBaseV1Factory {
-    function protocolAddresses(address _pair) external returns (address);
-    function spiritMaker() external returns (address);
-    function stableFee() external returns (uint256);
-    function variableFee() external returns (uint256);
-}
