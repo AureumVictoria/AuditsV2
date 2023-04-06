7e52321f1e944c1eb0b826d4116400f0b10da295
AureumVictoria <54036863+AureumVictoria@users.noreply.github.com>
2023-04-06 10:43:06 +0200

    Update BribeFactory.sol

--- Audit/bribe_factory/BribeFactory.sol
+++ Audit/bribe_factory/BribeFactory.sol
@@ -25,6 +25,9 @@ contract BribeFactory is IBribeFactory {
         lastBribe.addRewardtoken(_token0);
         lastBribe.addRewardtoken(_token1);
         last_bribe = address(lastBribe);
+        emit CreateBribe(last_bribe, _token0, _token1);
         return last_bribe;
     }
+
+    event CreateBribe(address last_bribe, address token0, address token1);
 }
